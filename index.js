06247645ee42b2fff6856bfcb6d18ebb13091305
mahmud <picowf@gmail.com>
2021-12-18 22:01:20 +0600

    done

--- index.js
+++ index.js
@@ -24,34 +24,14 @@ async function run() {
     const serviceCollection = database.collection("services");
     const orderCollection = database.collection("orders");
     const testimonialCollection = database.collection("testimonials");
+    const userCollection = database.collection("users");
 
+    // service
     // get services
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find({});
       const result = await cursor.toArray();
       res.send(result);
-    });
-
-    // get testimonials
-    app.get("/testimonials", async (req, res) => {
-      const cursor = testimonialCollection.find({});
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    // post data to server
-    // sending order info to the server
-    app.post("/servicesOrder", async (req, res) => {
-      const servicesOrder = req.body;
-      const result = await orderCollection.insertOne(servicesOrder);
-      res.json(result);
-    });
-
-    // send testimonial to database
-    app.post("/testimonials", async (req, res) => {
-      const testimonial = req.body;
-      const result = await testimonialCollection.insertOne(testimonial);
-      res.json(result);
     });
 
     // send service to database
@@ -61,6 +41,22 @@ async function run() {
       res.json(result);
     });
 
+    // testimonial
+    // get testimonials
+    app.get("/testimonials", async (req, res) => {
+      const cursor = testimonialCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // send testimonial to database
+    app.post("/testimonials", async (req, res) => {
+      const testimonial = req.body;
+      const result = await testimonialCollection.insertOne(testimonial);
+      res.json(result);
+    });
+
+    // order
     // get all orders
     app.get("/allorders", async (req, res) => {
       const cursor = orderCollection.find({});
@@ -77,6 +73,83 @@ async function run() {
       res.send(result);
     });
 
+    // sending order info to the server
+    app.post("/servicesOrder", async (req, res) => {
+      const servicesOrder = req.body;
+      const result = await orderCollection.insertOne(servicesOrder);
+      res.json(result);
+    });
+
+    //user
+    // sending user to databse
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.json(result);
+    });
+
+    // sending google user to database
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    // adding admin to userCollection
+    // add role to database
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+
+    // get special one to make admin
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // delete
+    // delete one order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // delete one service
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // update shipping status
+    app.put("/orders/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = { $set: { status: "shipped" } };
+      const result = await orderCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
     // final
   } finally {
     // await client.close();
